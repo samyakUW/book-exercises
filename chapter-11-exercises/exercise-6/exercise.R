b490bb8f9f4297ab63b75e73f33b635c507545a4
Samyak Jain <samyak26@uw.edu>
2020-02-07 21:22:31 -0800

    Week 5 exercises

--- chapter-11-exercises/exercise-6/exercise.R
+++ chapter-11-exercises/exercise-6/exercise.R
@@ -2,17 +2,28 @@
 
 # Install the `"nycflights13"` package. Load (`library()`) the package.
 # You'll also need to load `dplyr`
-#install.packages("nycflights13")  # should be done already
+# install.packages("nycflights13")  # should be done already
 library("nycflights13")
 library("dplyr")
 
-# Create a dataframe of the average arrival delays for each _destination_, then 
+# Create a dataframe of the average arrival delays for each _destination_, then
 # use `left_join()` to join on the "airports" dataframe, which has the airport
 # information
 # Which airport had the largest average arrival delay?
-
+largest_arrival_delay <- flights %>%
+  group_by(dest) %>%
+  summarise(avg_delay = mean(arr_delay, na.rm = TRUE)) %>%
+  mutate(faa = dest) %>%
+  left_join(airports, by = "faa") %>%
+  filter(avg_delay == max(avg_delay, na.rm = TRUE)) %>% 
+  print()
 
 # Create a dataframe of the average arrival delay for each _airline_, then use
 # `left_join()` to join on the "airlines" dataframe
 # Which airline had the smallest average arrival delay?
-
+smallest_airline_delay <- flights %>%
+  group_by(carrier) %>%
+  summarise(avg_delay = mean(arr_delay, na.rm = TRUE)) %>%
+  left_join(airlines, by = "carrier") %>%
+  filter(avg_delay == max(avg_delay, na.rm = TRUE)) %>% 
+  print()
